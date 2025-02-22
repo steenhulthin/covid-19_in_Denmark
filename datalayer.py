647c09fb310d4b5386ec4bc166d2054099a49cf7
Steen Hulthin Rasmussen <steen@hulthin.dk>
2025-02-23 00:44:55 +0100

    stuff

--- datalayer.py
+++ datalayer.py
@@ -1,4 +1,21 @@
 import pandas as pd
+import requests
+from io import StringIO
 
 def get_confirmed_admitted_deceased_per_day_per_sex():
-    return pd.read_csv(r'./data/03_bekraeftede_tilfaelde_doede_indlagte_pr_dag_pr_koen.csv', delimiter=';')
+    url = "https://steenhulthin.github.io/infectious-diseases-data/03_bekraeftede_tilfaelde_doede_indlagte_pr_dag_pr_koen.csv"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = StringIO(response.text)
+        return pd.read_csv(data, delimiter=';')
+    else:
+        print("Failed to download CSV file. Status code:", response.status_code)
+    
+    #return pd.read_csv(r'./data/03_bekraeftede_tilfaelde_doede_indlagte_pr_dag_pr_koen.csv', delimiter=';')
+    #return pd.read_csv(r'./data/03_bekraeftede_tilfaelde_doede_indlagte_pr_dag_pr_koen.csv', delimiter=';')
+
+def get_some_rando_stuff():
+    return 'rando stuff'
+
+
